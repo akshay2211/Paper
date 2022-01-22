b1d3d2527738c6785fc16ed5a854ac6e2e52704f
Akshay Sharma <fxn769@gmail.com>
2022-01-22 23:23:07 +0530

    drawbox updated

--- app/src/main/java/io/ak1/paper/ui/screens/doodle/DoodleScreen.kt
+++ app/src/main/java/io/ak1/paper/ui/screens/doodle/DoodleScreen.kt
@@ -1,23 +1,20 @@
 package io.ak1.paper.ui.screens.doodle
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import io.ak1.drawbox.DrawBox
+import io.ak1.drawbox.rememberDrawController
 import io.ak1.paper.R
+import io.ak1.paper.ui.component.PaperIconButton
+import io.ak1.paper.ui.screens.Destinations
+
 
 /**
  * Created by akshay on 01/01/22
@@ -25,54 +22,43 @@ import io.ak1.paper.R
  */
 @Composable
 fun DoodleScreen(navController: NavHostController) {
+
+    //val history = remember{ mutableStateOf<>()}
+    val drawController = rememberDrawController()
+
     Column {
         TopAppBar(
-            title = {
-                Text(text = "Doodle", color = MaterialTheme.colors.primary)
-            },
-            modifier = Modifier.fillMaxWidth(),
+            title = { Text(text = "Doodle", color = MaterialTheme.colors.primary) },
             navigationIcon = {
-                Image(
-                    painter = painterResource(R.drawable.ic_back),
-                    contentDescription = stringResource(id = R.string.image_desc),
-                    colorFilter = ColorFilter.tint(MaterialTheme.colors.primary),
-                    modifier = Modifier
-                        .clickable {
-                            navController.navigateUp()
-                        }
-                        .padding(12.dp)
-                )
+                PaperIconButton(id = R.drawable.ic_back) {
+                    navController.navigateUp()
+                }
             },
             actions = {
-                Image(
-                    painter = painterResource(R.drawable.ic_check),
-                    contentDescription = stringResource(id = R.string.image_desc),
-                    colorFilter = ColorFilter.tint(MaterialTheme.colors.primary),
-                    modifier = Modifier
-                        .clickable {
-                            navController.navigateUp()
-                        }
-                        .padding(12.dp)
-                )
-                Image(
-                    painter = painterResource(R.drawable.ic_trash),
-                    contentDescription = stringResource(id = R.string.image_desc),
-                    colorFilter = ColorFilter.tint(MaterialTheme.colors.primary),
-                    modifier = Modifier
-                        .clickable {
-                            navController.navigateUp()
-                        }
-                        .padding(12.dp)
-                )
+                PaperIconButton(
+                    id = R.drawable.ic_check,
+                ) {
+                    navController.navigateUp()
+                }
+                PaperIconButton(
+                    id = R.drawable.ic_trash,
+                ) {
+                    //drawController.getDrawBoxBitmap()?.getEncodedString()
+                    navController.popBackStack(Destinations.NOTE_ROUTE, true, false)
+
+                }
             },
-            elevation = 0.dp,
-            backgroundColor = MaterialTheme.colors.background
+            backgroundColor = MaterialTheme.colors.background,
+            elevation = 0.dp
         )
         DrawBox(
+            drawController = drawController,
             Modifier
                 .fillMaxSize()
                 .weight(1f, true)
-        )
+        ) { one, two ->
+
+        }
     }
 
 }
