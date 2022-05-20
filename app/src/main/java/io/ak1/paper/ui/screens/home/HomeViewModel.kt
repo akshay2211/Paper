7f5cf81bd45995745be2b84b3c0a39b3ba5cf630
Akshay Sharma <fxn769@gmail.com>
2022-05-20 17:15:15 +0530

    SearchScreen.kt functionality updated

--- app/src/main/java/io/ak1/paper/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/io/ak1/paper/ui/screens/home/HomeViewModel.kt
@@ -1,16 +1,9 @@
 package io.ak1.paper.ui.screens.home
 
-import android.content.Context
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import io.ak1.paper.R
-import io.ak1.paper.data.local.AppDatabase
 import io.ak1.paper.data.repositories.local.LocalRepository
 import io.ak1.paper.data.repositories.notes.NotesRepository
-import io.ak1.paper.models.Doodle
-import io.ak1.paper.models.Image
-import io.ak1.paper.models.Note
 import io.ak1.paper.models.NoteWithDoodleAndImage
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -31,8 +24,6 @@ data class HomeUiState(
 const val DEFAULT = "default"
 
 class HomeViewModel(
-    private val db: AppDatabase,
-    private val context: Context,
     private val localRepository: LocalRepository,
     private val notesRepository: NotesRepository
 ) : ViewModel() {
@@ -56,83 +47,14 @@ class HomeViewModel(
             localRepository.saveCurrentNote(currentNoteId)
         }
     }
+
     fun saveCurrentNote() {
         viewModelScope.launch {
             localRepository.saveCurrentNote()
         }
     }
 
-
-
-    val noteDao = db.noteDao()
-    val doodleDao = db.doodleDao()
-    val imageDao = db.imageDao()
-
-    private suspend fun getNotesCount() = noteDao.getNotesCountByFolderId(DEFAULT)
-
-    fun getAllNotesByDescription(query: String) = if (query.trim().isEmpty()) MutableLiveData(
-        emptyList()
-    ) else noteDao.getNotesBySearch(query)
-
-
-    fun insertDefaultData() {
-        viewModelScope.launch {
-            val count = getNotesCount()
-            if (count == 0) {
-                noteDao.insert(
-                    Note(
-                        description = context.resources.getString(R.string.default_note_text),
-                        folderId = DEFAULT
-                    )
-                )
-                noteDao.insert(
-                    Note(
-                        description = context.resources.getString(R.string.default_note_text2),
-                        folderId = DEFAULT
-                    )
-                )
-                noteDao.insert(
-                    Note(
-                        description = context.resources.getString(R.string.default_note_text3),
-                        folderId = DEFAULT
-                    )
-                )
-            }
-        }
-    }
-
-
-    fun saveNote(note: Note) = viewModelScope.launch {
-        noteDao.insert(note = note)
-    }
-
-    fun saveDoodle(vararg doodle: Doodle) = viewModelScope.launch {
-        doodleDao.insertAll(doodle = doodle)
-    }
-
-    fun saveDoodle(doodles: ArrayList<Doodle>, noteId: String) {
-        if (doodles.isNotEmpty()) {
-            val newDoodles = doodles.map {
-                it.attachedNoteId = noteId
-                it
-            }.toTypedArray()
-            saveDoodle(*newDoodles)
-        }
-    }
-
-    fun saveImage(vararg image: Image) = viewModelScope.launch {
-        imageDao.insertAll(image = image)
-    }
-
-    fun saveImage(images: ArrayList<Image>, noteId: String) {
-        if (images.isNotEmpty()) {
-            val newImages = images.map {
-                it.attachedNoteId = noteId
-                it
-            }.toTypedArray()
-            saveImage(*newImages)
-        }
-    }
+    fun getAllNotesByDescription(query: String) = notesRepository.getNotesBySearch(query)
 
 
 }
