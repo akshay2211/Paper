fafae331a0bcd3856ce98e28cb9e5b750180fdac
Akshay Sharma <fxn769@gmail.com>
2022-05-25 23:56:36 +0530

    Update OptionsScreen.kt
    extra padding removed

--- app/src/main/java/io/ak1/paper/ui/screens/note/options/OptionsScreen.kt
+++ app/src/main/java/io/ak1/paper/ui/screens/note/options/OptionsScreen.kt
@@ -53,7 +53,7 @@ fun OptionsScreen(navigateTo: (String) -> Unit, backPress: () -> Unit) {
         focusedElevation = 0.dp
     )
     val colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent)
-    LazyColumn(modifier = Modifier.padding(12.dp)) {
+    LazyColumn(modifier = Modifier.padding(3.dp, 12.dp)) {
         items(list) { it ->
             Button(
                 onClick = {
